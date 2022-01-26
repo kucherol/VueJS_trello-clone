4130e76777040d847a7e65facc691d59977d5f33
kucherol <kucherol2108@gmail.com>
2022-01-26 16:41:02 +0200

    board delete fixed

--- src/components/ViewComponents/BoardComponent/DeleteBoard.vue
+++ src/components/ViewComponents/BoardComponent/DeleteBoard.vue
@@ -8,7 +8,7 @@
             </v-list-item>
             <v-list-item class="control">
                 <v-list-item-action class="control-list">
-                    <v-btn icon large @click="deleteBoard">
+                    <v-btn icon large @click.prevent="deleteBoard">
                         <v-icon class="icon-ok">mdi-check</v-icon>
                     </v-btn>
                     <v-btn icon large @click="closeDeleteBoard">
@@ -25,6 +25,12 @@ import firebase from "../../../firebase.js";
 import { mapGetters, mapActions } from "vuex";
 export default {
     name: "DeleteBoard",
+	data() {
+		return {
+			lists: [],
+			cards: [],
+		}
+	},
     props: {
         id: {
             type: String,
@@ -35,7 +41,46 @@ export default {
         closeDeleteBoard() {
             this.$emit("closeDeleteBoard", this.id);
         },
+		getLists() {
+			firebase.firestore().collection('users').doc(this.user.id).collection("boards").doc(this.id).collection("lists").get()
+			.then(response => {
+				response.forEach((doc) => {
+					this.lists.push({
+						id: doc.id,
+						title: doc.data().title,
+						sortListIndex: doc.data().sortListIndex,
+					})
+				})
+				this.getCards();
+			})
+		},
+		getCards() {
+			for (let i = 0; i < this.lists.length; i++) {
+				firebase.firestore().collection('users').doc(this.user.id).collection("boards").doc(this.id).collection("lists").doc(this.lists[i].id).collection("cards").get()
+				.then(response => {
+					response.forEach((doc) => {
+						this.cards.push({
+							listID: this.lists[i].id,
+							id: doc.id,
+							title: doc.data().title,
+							information: doc.data().information,
+							sortCardIndex: doc.data().sortCardIndex,
+						})
+					})
+				})
+			}
+		},
+
         deleteBoard() {
+			for (let i = 0; i < this.lists.length; i++) {
+				console.log(this.lists.length, i)
+				for (let j = 0; j < this.cards.length; j++) {
+					firebase.firestore().collection('users').doc(this.user.id).collection("boards").doc(this.id).collection("lists").doc(this.lists[i].id).collection("cards").doc(this.cards[j].id).delete()
+					.catch((error) => {console.log(error)});
+				}
+				firebase.firestore().collection('users').doc(this.user.id).collection("boards").doc(this.id).collection("lists").doc(this.lists[i].id).delete()
+				.catch((error) => {console.log(error)});
+			}
             firebase.firestore().collection("users").doc(this.user.id).collection("boards").doc(this.id).delete()
 			.then(() => {
 				this.getBoardsList(this.$route.params.dashboardId);
@@ -48,7 +93,10 @@ export default {
     },
 	computed: {
 		...mapGetters(["user"])
-	}
+	},
+	created() {
+		this.getLists();
+	},
 }
 </script>
 
